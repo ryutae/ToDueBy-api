4f0fc86e8e4cb5f78926802092e9c4a1d3c1d3ae
Ryuta Eguchi <ryuta.eguchi@gmail.com>
2019-08-20 15:12:57 -0500

    Edit config

--- src/config.js
+++ src/config.js
@@ -3,5 +3,6 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DATABASE_URL || 'postgresql://Ryuta@localhost/todueby',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
+    "http://localhost:8000/api"
 }
-
