4f8c9c073041dd17080bb708cf2e2bb1247a7fb7
Ryuta Eguchi <ryuta.eguchi@gmail.com>
2019-08-20 09:49:05 -0500

    Modify for deployment

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://Ryuta@localhost/todueby',
+  DB_URL: process.env.DATABASE_URL || 'postgresql://Ryuta@localhost/todueby',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 }
 
